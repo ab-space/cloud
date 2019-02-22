bb0949799cecc23473d73975561c0decf8eea851
tianky <412138965@qq.com>
2019-02-22 16:02:34 +0800

    euerka commit

--- cloud-base-provider/euerka-server/src/main/java/indi/wm/cloud/euerka/EuerkaApplication.java
+++ cloud-base-provider/euerka-server/src/main/java/indi/wm/cloud/euerka/EuerkaApplication.java
@@ -1,8 +1,14 @@
 package indi.wm.cloud.euerka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class EuerkaApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(EuerkaApplication.class, args);
     }
 }
